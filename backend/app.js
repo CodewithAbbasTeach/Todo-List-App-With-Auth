a4a8d55c6ea8c6870b7b59a22159e9d205a97a4b
Your Name <you@example.com>
2024-09-27 20:03:43 +0500

    today add in this project sign up/sign in functionaly /use bcryptjs for password encrypt

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 const express = require("express"); // Fixed spelling of 'express'
+const app = express();
+require("./conn/conn"); // Fixed spelling of 'express'
+const auth= require("./routes/auth")
 
 
-const app = express(); // Fixed spelling of 'express'
-require("./conn/conn");
+app.use(express.json());
 app.get("/", (req, res) => {
     res.send("hello world");
 });
+
+app.use("/api/v1/",auth);
+
+
 
 app.listen(1000, () => {
     console.log("server started on port 1000"); // Added port number to the log
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,41 @@
 const router=require("express").Router();
-
 const User =require("../models/user");
+const bcrypt= require("bcryptjs")
 
 
-router.post("/register", async(req,res)=>{
+router.post("/register", async(req, res)=>{
     try{
-       const {email, username , password} =req.body
+       const {email, username , password} =req.body;
+       const hashpassword=bcrypt.hashSync(password);
+       const user=new User({email, username, password: hashpassword});
+       await user.save().then(()=>
+        res.status(200).json({user:user})
+       );
     }
     catch(error){
+          res.status(400).json({message:"User Already Exits"})
+    }
+});
 
+router.post("/signin", async(req, res)=>{
+    try{
+         const user=await User.findOne({email: req.body.email });
+         if(!user){
+            res.status(400).json({message:"Please Sign Up First"})
+         }
+         const IspasswordCorrect=bcrypt.compareSync(
+            req.body.password, 
+            user.password
+        );
+         if(!IspasswordCorrect){
+            console.log(error)
+            res.status(400).json({message: "Please is not Correct Password"})
+         }
+        const { password, ...others }=user._doc;
+        res.status(200).json({others})
+    }
+    catch(error){
+          res.status(400).json({message: "user not found"})
     }
 })
-
 module.exports=router
